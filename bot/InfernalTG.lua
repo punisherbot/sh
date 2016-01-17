1f45d6dc794fd0808cc1d64160f986a937dd12c4
punisherbot <absam14339@yahoo.com>
2016-01-17 19:29:09 +0330

    Update InfernalTG.lua

--- bot/InfernalTG.lua
+++ bot/InfernalTG.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tagall",
     
     },
-    sudo_users = {109722284,99743635,171604508},--Sudo users
+    sudo_users = {168398326},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[infernalTG  v2 - Open Source
